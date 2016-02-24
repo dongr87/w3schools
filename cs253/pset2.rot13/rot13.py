8c7e905cc22cb57a7a9b3fecb56cba44e85704d0
Dong Wang <mtwcore@gmail.com>
2016-02-23 19:51:19 -0500

    pset2

---
+++ cs253/pset2.rot13/rot13.py
@@ -0,0 +1,52 @@
+import webapp2
+import cgi
+
+def escape_html(s):
+    return cgi.escape(s, quote=True)
+
+def rot13(s):
+    result = ''
+    for v in s:
+        c = ord(v)
+        if c <= ord('a') and c >= ord('z'):
+            if c > ord('m'):
+                c -= 13
+            else:
+                c += 13
+        elif c <= ord('A') and c >= ord('Z'):
+            if c> ord('M'):
+                c -= 13
+            else:
+                c += 13
+        result += chr(c)
+
+    return result
+
+html = """
+<h1>Enter some text to ROT13:</h1>
+<form method="post">
+<textarea name="text"  cols="36" rows="10" placeholder="Please enter here..." value="%(user_text)s" style="border: 1px solid blue; padding: 15px 18px; font-size: 1.2em;">
+</textarea>
+<br>
+<div style="color: red;">%(error)s</div>
+<br>
+<input type="submit" style="background-color: #2F648C; padding: 3px 8px; width: 200px; height: 40px; text-align: center; color: #FFFFFF; margin-left: 90px;">
+</form>
+"""
+
+class ROT13(webapp2.RequestHandler):
+    def write_html(self, user_text =""):
+        self.response.out.write(html % {"text": user_text})
+
+    def get(self):
+        self.write_html()
+
+    def post(self):
+        user_text = self.request.get('text')
+        if user_text:
+            self.write_html('Succeed!', rot13(user_text))
+        else:
+            self.write_html()
+
+app = webapp2.WSGIApplication([('/', ROT13)],
+                              debug = True)
