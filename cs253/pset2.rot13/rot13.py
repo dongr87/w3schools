68b12cdaf1c1bd682e1a250fb66cbcc1de62efc9
Dong Wang <mtwcore@gmail.com>
2016-02-23 21:57:29 -0500

    feb23

--- cs253/pset2.rot13/rot13.py
+++ cs253/pset2.rot13/rot13.py
@@ -8,12 +8,12 @@ def rot13(s):
     result = ''
     for v in s:
         c = ord(v)
-        if c <= ord('a') and c >= ord('z'):
+        if c >= ord('a') and c <= ord('z'):
             if c > ord('m'):
                 c -= 13
             else:
                 c += 13
-        elif c <= ord('A') and c >= ord('Z'):
+        elif c >= ord('A') and c <= ord('Z'):
             if c> ord('M'):
                 c -= 13
             else:
@@ -25,7 +25,7 @@ def rot13(s):
 html = """
 <h1>Enter some text to ROT13:</h1>
 <form method="post">
-<textarea name="text"  cols="36" rows="10" placeholder="Please enter here..." value="%(user_text)s" style="border: 1px solid blue; padding: 15px 18px; font-size: 1.2em;">
+<textarea name="text"  cols="36" rows="10" placeholder="Please enter here..." value="%(text)s" style="border: 1px solid blue; padding: 15px 18px; font-size: 1.2em;">
 </textarea>
 <br>
 <div style="color: red;">%(error)s</div>
@@ -35,16 +35,18 @@ html = """
 """
 
 class ROT13(webapp2.RequestHandler):
-    def write_html(self, user_text =""):
-        self.response.out.write(html % {"text": user_text})
+    def write_html(self, error = "", text =""):
+        self.response.out.write(html % {"error": error,
+                                "text": text})
 
     def get(self):
         self.write_html()
 
     def post(self):
         user_text = self.request.get('text')
+        transformed_text = rot13(user_text)
         if user_text:
-            self.write_html('Succeed!', rot13(user_text))
+            self.write_html(transformed_text, transformed_text)
         else:
             self.write_html()
 
